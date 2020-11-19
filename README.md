7a9da7099dfbe713658edfd6657145beecc30870
Andrei Solntsev <andrei.solntsev@gmail.com>
2020-11-20 00:28:08 +0200

    initial project

---
+++ README.md
@@ -0,0 +1,16 @@
+Selenide examples: Selenoid
+================================
+
+This is a sample project demonstrating how to run Selenide UI tests with browsers in Selenoid. 
+
+**You can checkout and run it locally with a few minutes.**
+
+### How to run with Maven
+
+Type from command line to run tests on your machine:
+
+```
+mvn test
+```
+
+_Feel free to share your feedback!_
---
+++ src/test/java/org/selenide/examples/selenoid/FileDownloadTest.java
@@ -0,0 +1,46 @@
+package org.selenide.examples.selenoid;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.FileDownloadMode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.files.FileFilters.withExtension;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.apache.commons.io.FileUtils.readFileToString;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FileDownloadTest {
+  @BeforeEach
+  void setUp() {
+    Configuration.remote = "http://localhost:4444/wd/hub";
+    Configuration.fileDownload = FileDownloadMode.FOLDER;
+    Configuration.reportsFolder = "target/surefire-reports";
+    Configuration.downloadsFolder = "target/downloads";
+
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setBrowserName("chrome");
+    capabilities.setVersion("86.0");
+    capabilities.setCapability("enableVNC", true);
+    capabilities.setCapability("enableVideo", true);
+    capabilities.setCapability("enableLog", true);
+    Configuration.browserCapabilities = capabilities;
+  }
+
+  @Test
+  void download() throws IOException {
+    open("https://the-internet.herokuapp.com/download");
+
+    File file = $(byText("some-file.txt")).download(withExtension("txt"));
+
+    assertThat(file.getName()).isEqualTo("some-file.txt");
+    assertThat(readFileToString(file, UTF_8)).startsWith("{\\rtf");
+  }
+}
