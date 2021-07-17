ced7306718134ec0ad75a555c267aa005998af09
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-07-17 11:35:02 +0300

    upgrade to Selenide 5.23.0 and selenide-selenoid 1.1.4

--- src/test/java/org/selenide/examples/selenoid/ClipboardTest.java
+++ src/test/java/org/selenide/examples/selenoid/ClipboardTest.java
@@ -2,10 +2,14 @@ package org.selenide.examples.selenoid;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.selenide.selenoid.SelenoidClipboard;
 
+import static com.codeborne.selenide.ClipboardConditions.content;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.clipboard;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.sleep;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SelenoidExtension.class)
@@ -13,15 +17,19 @@ public class ClipboardTest {
   @Test
   void canGetClipboardContent() {
     open("https://github.com/selenide/selenide");
-    $(".file-navigation get-repo details.details-overlay").click();
-    $(".octicon-clippy").click();
-    assertThat(clipboard().getText()).isEqualTo("https://github.com/selenide/selenide.git");
+    $(".file-navigation get-repo details.details-overlay").shouldBe(visible).click();
+    sleep(500);
+    $(".octicon-clippy").shouldBe(visible).click();
+    assertThat(clipboard()).isInstanceOf(SelenoidClipboard.class);
+    clipboard().shouldHave(content("https://github.com/selenide/selenide.git"));
   }
 
   @Test
   void canSetClipboardContent() {
     open("https://github.com/selenide/selenide");
+    assertThat(clipboard()).isInstanceOf(SelenoidClipboard.class);
     clipboard().setText("что такое осень");
-    assertThat(clipboard().getText()).isEqualTo("что такое осень");
+    sleep(500);
+    clipboard().shouldHave(content("что такое осень"));
   }
 }
--- src/test/java/org/selenide/examples/selenoid/SelenoidExtension.java
+++ src/test/java/org/selenide/examples/selenoid/SelenoidExtension.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class SelenoidExtension implements BeforeAllCallback  {
   @Override
   public void beforeAll(ExtensionContext context) {
@@ -12,12 +15,14 @@ class SelenoidExtension implements BeforeAllCallback  {
     Configuration.reportsFolder = "target/surefire-reports";
     Configuration.downloadsFolder = "target/downloads";
 
+    Map<String, Boolean> options = new HashMap<>();
+    options.put("enableVNC", true);
+    options.put("enableVideo", true);
+    options.put("enableLog", true);
     DesiredCapabilities capabilities = new DesiredCapabilities();
     capabilities.setBrowserName("chrome");
     capabilities.setVersion("86.0");
-    capabilities.setCapability("enableVNC", true);
-    capabilities.setCapability("enableVideo", true);
-    capabilities.setCapability("enableLog", true);
+    capabilities.setCapability("selenoid:options", options);
     Configuration.browserCapabilities = capabilities;
   }
 }
