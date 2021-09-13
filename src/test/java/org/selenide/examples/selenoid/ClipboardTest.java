fe48abe6007af2e4f6e5e11748a0ab6f72d7684c
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-09-13 22:18:59 +0300

    upgrade to Selenide 5.24.3 and selenide-selenoid 1.1.7

--- src/test/java/org/selenide/examples/selenoid/ClipboardTest.java
+++ src/test/java/org/selenide/examples/selenoid/ClipboardTest.java
@@ -1,27 +1,38 @@
 package org.selenide.examples.selenoid;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.selenide.selenoid.SelenoidClipboard;
 
 import static com.codeborne.selenide.ClipboardConditions.content;
+import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.clipboard;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.sleep;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SelenoidExtension.class)
 public class ClipboardTest {
+  @BeforeEach
+  public void prepare() {
+    open("https://www.w3schools.com/howto/howto_js_copy_clipboard.asp");
+    $(".snigel-cmp-framework").shouldBe(visible);
+    executeJavaScript("const popup = document.getElementById('snigel-cmp-framework'); popup.parentNode.removeChild(popup);");
+  }
+
   @Test
   void canGetClipboardContent() {
-    open("https://github.com/selenide/selenide");
-    $(".file-navigation get-repo details.details-overlay").shouldBe(visible).click();
-    sleep(500);
-    $(".octicon-clippy").shouldBe(visible).click();
-    assertThat(clipboard()).isInstanceOf(SelenoidClipboard.class);
-    clipboard().shouldHave(content("https://github.com/selenide/selenide.git"));
+    $("#myInput")
+        .shouldHave(attribute("value", "Hello World"))
+        .val("Dexter Morgan Freeman");
+    $("button").click();
+    clipboard().shouldHave(content("Dexter Morgan Freeman"));
+    assertEquals("Dexter Morgan Freeman", clipboard().getText());
   }
 
   @Test
