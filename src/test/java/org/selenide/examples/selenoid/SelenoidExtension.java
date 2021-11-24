bc0fcbcfd02b7e8bddfeb2d276dce571c9cdc877
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-11-24 21:55:57 +0200

    upgrade to Selenide 6.1.1 and selenide-selenoid 2.1.0

--- src/test/java/org/selenide/examples/selenoid/SelenoidExtension.java
+++ src/test/java/org/selenide/examples/selenoid/SelenoidExtension.java
@@ -3,7 +3,7 @@ package org.selenide.examples.selenoid;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,10 +19,8 @@ class SelenoidExtension implements BeforeAllCallback  {
     options.put("enableVNC", true);
     options.put("enableVideo", true);
     options.put("enableLog", true);
-    DesiredCapabilities capabilities = new DesiredCapabilities();
-    capabilities.setBrowserName("chrome");
-    capabilities.setVersion("86.0");
-    capabilities.setCapability("selenoid:options", options);
-    Configuration.browserCapabilities = capabilities;
+
+    Configuration.browserCapabilities = new ChromeOptions();
+    Configuration.browserCapabilities.setCapability("selenoid:options", options);
   }
 }
